8fd663b3559dc378a92cace2ff22798ac1d53321
Jamal Bhatti <jamal136@hotmail.com>
2020-10-21 11:59:08 +0100

    AddressLine55

--- AdventureWorks2016/AdventureWorks2016/Person/Tables/Address.sql
+++ AdventureWorks2016/AdventureWorks2016/Person/Tables/Address.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Person].[Address] (
     [AddressID]       INT               IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
     [AddressLine1]    NVARCHAR (60)     NOT NULL,
-    [AddressLine2]    NVARCHAR (60)     NULL,
+    [AddressLine55]    NVARCHAR (60)     NULL,
     [City]            NVARCHAR (30)     NOT NULL,
     [StateProvinceID] INT               NOT NULL,
     [PostalCode]      NVARCHAR (15)     NOT NULL,
