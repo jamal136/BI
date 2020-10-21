0da8b6afed8783be33ba3d1e351b821b63108362
Jamal Bhatti <jamal136@hotmail.com>
2020-10-21 11:04:17 +0100

    1

---
+++ AdventureWorks2016/AdventureWorks2016/dbo/Tables/Department.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [DeptID]       INT                                         NOT NULL,
+    [DeptName]     VARCHAR (60)                                NOT NULL,
+    [DeptName2]    VARCHAR (60)                                NULL,
+    [DeptName3]    VARCHAR (60)                                NULL,
+    [ManagerID]    INT                                         NULL,
+    [ParentDeptID] INT                                         NULL,
+    [SysStartTime] DATETIME2 (7) GENERATED ALWAYS AS ROW START NOT NULL,
+    [SysEndTime]   DATETIME2 (7) GENERATED ALWAYS AS ROW END   NOT NULL,
+    PRIMARY KEY CLUSTERED ([DeptID] ASC),
+    PERIOD FOR SYSTEM_TIME ([SysStartTime], [SysEndTime])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[dbo].[DepartmentHistory], DATA_CONSISTENCY_CHECK=ON));
+
---
+++ AdventureWorks2016/AdventureWorks2016/dbo/Tables/DepartmentHistory.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[DepartmentHistory] (
+    [DeptID]       INT           NOT NULL,
+    [DeptName]     VARCHAR (60)  NOT NULL,
+    [DeptName2]    VARCHAR (60)  NULL,
+    [DeptName3]    VARCHAR (60)  NULL,
+    [ManagerID]    INT           NULL,
+    [ParentDeptID] INT           NULL,
+    [SysStartTime] DATETIME2 (7) NOT NULL,
+    [SysEndTime]   DATETIME2 (7) NOT NULL
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_DepartmentHistory_ID_PERIOD_COLUMNS]
+    ON [dbo].[DepartmentHistory]([SysEndTime] ASC, [SysStartTime] ASC, [DeptID] ASC);
+
+
+GO
+CREATE CLUSTERED COLUMNSTORE INDEX [IX_DepartmentHistory]
+    ON [dbo].[DepartmentHistory];
+
