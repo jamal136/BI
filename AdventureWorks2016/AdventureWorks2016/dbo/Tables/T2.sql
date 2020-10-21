ac2ee9ee3cb3e8a26289e4065c0febee6d94452e
Jamal Bhatti <jamal136@hotmail.com>
2020-10-21 11:48:18 +0100

    Rm Address2 tbl

---
+++ AdventureWorks2016/AdventureWorks2016/dbo/Tables/T2.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[T2] (
+    [BusinessEntityID]  INT                 NOT NULL,
+    [NationalIDNumber]  NVARCHAR (15)       NOT NULL,
+    [LoginID]           NVARCHAR (256)      NOT NULL,
+    [OrganizationNode]  [sys].[hierarchyid] NULL,
+    [OrganizationLevel] SMALLINT            NULL,
+    [JobTitle]          NVARCHAR (50)       NOT NULL,
+    [BirthDate]         DATE                NOT NULL,
+    [MaritalStatus]     NCHAR (1)           NOT NULL,
+    [Gender]            NCHAR (1)           NOT NULL,
+    [HireDate]          DATE                NOT NULL,
+    [SalariedFlag]      [dbo].[Flag]        NOT NULL,
+    [VacationHours]     SMALLINT            NOT NULL,
+    [SickLeaveHours]    SMALLINT            NOT NULL,
+    [CurrentFlag]       [dbo].[Flag]        NOT NULL,
+    [rowguid]           UNIQUEIDENTIFIER    NOT NULL,
+    [ModifiedDate]      DATETIME            NOT NULL
+);
+
